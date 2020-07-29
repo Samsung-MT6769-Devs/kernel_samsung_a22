d67c4902e3c7197c33a38d657f33ff8f9faca3d4
Greg Kroah-Hartman <gregkh@google.com>
2020-07-29 09:11:55 +0200

    Merge 4.14.190 into android-4.14-stable
    Changes in 4.14.190
    	gpio: arizona: handle pm_runtime_get_sync failure case
    	gpio: arizona: put pm_runtime in case of failure
    	pinctrl: amd: fix npins for uart0 in kerncz_groups
    	mac80211: allow rx of mesh eapol frames with default rx key
    	scsi: scsi_transport_spi: Fix function pointer check
    	xtensa: fix __sync_fetch_and_{and,or}_4 declarations
    	xtensa: update *pos in cpuinfo_op.next
    	drivers/net/wan/lapbether: Fixed the value of hard_header_len
    	net: sky2: initialize return of gm_phy_read
    	drm/nouveau/i2c/g94-: increase NV_PMGR_DP_AUXCTL_TRANSACTREQ timeout
    	irqdomain/treewide: Keep firmware node unconditionally allocated
    	SUNRPC reverting d03727b248d0 ("NFSv4 fix CLOSE not waiting for direct IO compeletion")
    	spi: spi-fsl-dspi: Exit the ISR with IRQ_NONE when it's not ours
    	IB/umem: fix reference count leak in ib_umem_odp_get()
    	uprobes: Change handle_swbp() to send SIGTRAP with si_code=SI_KERNEL, to fix GDB regression
    	ALSA: info: Drop WARN_ON() from buffer NULL sanity check
    	ASoC: rt5670: Correct RT5670_LDO_SEL_MASK
    	btrfs: fix double free on ulist after backref resolution failure
    	btrfs: fix mount failure caused by race with umount
    	btrfs: fix page leaks after failure to lock page for delalloc
    	bnxt_en: Fix race when modifying pause settings.
    	hippi: Fix a size used in a 'pci_free_consistent()' in an error handling path
    	ax88172a: fix ax88172a_unbind() failures
    	net: dp83640: fix SIOCSHWTSTAMP to update the struct with actual configuration
    	drm: sun4i: hdmi: Fix inverted HPD result
    	net: smc91x: Fix possible memory leak in smc_drv_probe()
    	bonding: check error value of register_netdevice() immediately
    	mlxsw: destroy workqueue when trap_register in mlxsw_emad_init
    	ipvs: fix the connection sync failed in some cases
    	i2c: rcar: always clear ICSAR to avoid side effects
    	bonding: check return value of register_netdevice() in bond_newlink()
    	serial: exar: Fix GPIO configuration for Sealevel cards based on XR17V35X
    	scripts/decode_stacktrace: strip basepath from all paths
    	HID: i2c-hid: add Mediacom FlexBook edge13 to descriptor override
    	HID: apple: Disable Fn-key key-re-mapping on clone keyboards
    	dmaengine: tegra210-adma: Fix runtime PM imbalance on error
    	Input: add `SW_MACHINE_COVER`
    	spi: mediatek: use correct SPI_CFG2_REG MACRO
    	regmap: dev_get_regmap_match(): fix string comparison
    	hwmon: (aspeed-pwm-tacho) Avoid possible buffer overflow
    	dmaengine: ioat setting ioat timeout as module parameter
    	Input: synaptics - enable InterTouch for ThinkPad X1E 1st gen
    	usb: gadget: udc: gr_udc: fix memleak on error handling path in gr_ep_init()
    	arm64: Use test_tsk_thread_flag() for checking TIF_SINGLESTEP
    	x86: math-emu: Fix up 'cmp' insn for clang ias
    	binder: Don't use mmput() from shrinker function.
    	usb: xhci-mtk: fix the failure of bandwidth allocation
    	usb: xhci: Fix ASM2142/ASM3142 DMA addressing
    	Revert "cifs: Fix the target file was deleted when rename failed."
    	staging: wlan-ng: properly check endpoint types
    	staging: comedi: addi_apci_1032: check INSN_CONFIG_DIGITAL_TRIG shift
    	staging: comedi: ni_6527: fix INSN_CONFIG_DIGITAL_TRIG support
    	staging: comedi: addi_apci_1500: check INSN_CONFIG_DIGITAL_TRIG shift
    	staging: comedi: addi_apci_1564: check INSN_CONFIG_DIGITAL_TRIG shift
    	serial: 8250: fix null-ptr-deref in serial8250_start_tx()
    	serial: 8250_mtk: Fix high-speed baud rates clamping
    	fbdev: Detect integer underflow at "struct fbcon_ops"->clear_margins.
    	vt: Reject zero-sized screen buffer size.
    	Makefile: Fix GCC_TOOLCHAIN_DIR prefix for Clang cross compilation
    	mm/memcg: fix refcount error while moving and swapping
    	io-mapping: indicate mapping failure
    	parisc: Add atomic64_set_release() define to avoid CPU soft lockups
    	ath9k: Fix general protection fault in ath9k_hif_usb_rx_cb
    	ath9k: Fix regression with Atheros 9271
    	Linux 4.14.190
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I0d395679325e47e1f916bc0aa64d6299563559f4

--- drivers/spi/spi-mt65xx.c
+++ drivers/spi/spi-mt65xx.c
@@ -47,7 +47,6 @@
 #define SPI_CFG0_SCK_LOW_OFFSET           8
 #define SPI_CFG0_CS_HOLD_OFFSET           16
 #define SPI_CFG0_CS_SETUP_OFFSET          24
-#define SPI_ADJUST_CFG0_SCK_LOW_OFFSET    16
 #define SPI_ADJUST_CFG0_CS_HOLD_OFFSET    0
 #define SPI_ADJUST_CFG0_CS_SETUP_OFFSET   16
 
@@ -426,7 +425,7 @@ static void mtk_spi_set_cs(struct spi_device *spi, bool enable)
 static void mtk_spi_prepare_transfer(struct spi_master *master,
 				     struct spi_transfer *xfer)
 {
-	u32 spi_clk_hz, div, sck_time, cs_time, reg_val = 0;
+	u32 spi_clk_hz, div, sck_time, cs_time, reg_val;
 	struct mtk_spi *mdata = spi_master_get_devdata(master);
 	u32 cs_setuptime, cs_holdtime, cs_idletime = 0;
 	struct mtk_chip_config *chip_config = spi->controller_data;
@@ -459,17 +458,15 @@ static void mtk_spi_prepare_transfer(struct spi_master *master,
 		reg_val = (((sck_time - 1) & 0xffff)
 			   << SPI_CFG2_SCK_HIGH_OFFSET);
 		reg_val |= (((sck_time - 1) & 0xffff)
-			   << SPI_CFG0_SCK_HIGH_OFFSET);
-		reg_val |= (((sck_time - 1) & 0xffff)
-			   << SPI_ADJUST_CFG0_SCK_LOW_OFFSET);
+			   << SPI_CFG2_SCK_LOW_OFFSET);
 		writel(reg_val, mdata->base + SPI_CFG2_REG);
-		reg_val |= (((cs_time - 1) & 0xffff)
+		reg_val = (((cs_time - 1) & 0xffff)
 			   << SPI_ADJUST_CFG0_CS_HOLD_OFFSET);
 		reg_val |= (((cs_time - 1) & 0xffff)
 			   << SPI_ADJUST_CFG0_CS_SETUP_OFFSET);
 		writel(reg_val, mdata->base + SPI_CFG0_REG);
 	} else {
-		reg_val |= (((sck_time - 1) & 0xff)
+		reg_val = (((sck_time - 1) & 0xff)
 			   << SPI_CFG0_SCK_HIGH_OFFSET);
 		reg_val |= (((sck_time - 1) & 0xff) << SPI_CFG0_SCK_LOW_OFFSET);
 		reg_val |= (((cs_time - 1) & 0xff) << SPI_CFG0_CS_HOLD_OFFSET);
