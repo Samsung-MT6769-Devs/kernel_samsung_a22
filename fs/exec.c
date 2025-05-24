5f15ec3e14a05d09b1803481981c44c3aa58277d
Zears14 <79321017+Zears14@users.noreply.github.com>
2025-05-24 17:05:37 +0700

    Fixed remaining diff indicator, and fixed redefinition on namespace.c
    Signed-off-by: Zears14 <79321017+Zears14@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1885,9 +1885,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 #ifdef CONFIG_KSU
 extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-								+			void *envp, int *flags);
+											void *envp, int *flags);
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-										 +				 void *argv, void *envp, int *flags);
+										 				 void *argv, void *envp, int *flags);
 #endif
 
 /*
