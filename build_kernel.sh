a5e87e3765820f30d2d853c27f498a849ffb7504
Zears14 <79321017+Zears14@users.noreply.github.com>
2025-05-24 15:51:03 +0700

    use newer cross compiler
    Signed-off-by: Zears14 <79321017+Zears14@users.noreply.github.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
+export CROSS_COMPILE=$(pwd)/toolchain/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
 export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
