2472924248543e25ec2cad1fd93e19048bc88b98
Zears14 <79321017+Zears14@users.noreply.github.com>
2025-05-28 23:10:32 +0700

    Improves kernel build and configuration
    This commit enhances the kernel build process and updates the default kernel configuration.
    
    - Updates .gitignore to include build output and toolchain directories.
    - Modifies the default kernel configuration to include WMK suffix.
    - Changes build_kernel.sh to continue on errors.
    - Cleans up and fixes various driver and file system issues.
    - Updates the defex rules.

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,6 +9,6 @@ export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a22_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+make -k -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
